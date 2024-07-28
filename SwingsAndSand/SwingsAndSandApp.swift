d804bada51425c050b0ce72d203b89b377ca2d7e
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-28 13:16:44 +0530

    Initial Commit

---
+++ SwingsAndSand/SwingsAndSandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingsAndSandApp.swift
+//  SwingsAndSand
+//
+//  Created by Mayur Vaity on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwingsAndSandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
